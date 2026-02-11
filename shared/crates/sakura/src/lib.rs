2f352f167f585e6a6ad8915cc4cb19db972e5953
suri-codes <suri312006@gmail.com>
2026-02-10 23:24:30 -0800

    feat(sakura): Init Sakura.
    Sakura aims to be a purpose built tree
    data-structure for CASE,
    that's compatible with AutoMerge,

---
+++ shared/crates/sakura/src/lib.rs
@@ -0,0 +1,29 @@
+//! A purpose-built library for CASE, to hold the internal tree-like
+//! data structure that holds tasks and groups. Additionally, this
+//! data structure is compatible with `AutoMerge`, via `AutoSurgeon`
+//!
+//! TODO: add example usage
+
+use autosurgeon::{Hydrate, Reconcile};
+use serde::{Deserialize, Serialize};
+
+pub use node::Node;
+
+/// A Node Id
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    Serialize,
+    Deserialize,
+    Reconcile,
+    Hydrate,
+)]
+pub struct NodeId {
+    index: u32,
+}
